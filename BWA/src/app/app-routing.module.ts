0503225deb45eac858e110536f9ed87c354a7f6a
Wesley Jones <wjones18@vols.utk.edu>
2019-06-28 15:33:25 -0400

    good progress

--- BWA/src/app/app-routing.module.ts
+++ BWA/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { QuestionComponent } from './question/question.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+  {path: '', pathMatch: 'full', component: HomeComponent},
+  {path: 'question', component: QuestionComponent},
+  {path: 'navbar', component: NavbarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- BWA/src/app/app.module.ts
+++ BWA/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { QuestionComponent } from './question/question.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
